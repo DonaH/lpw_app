cd8f22a147effe59b23b35eefe29e95e23998205
dona <dona.hsieh@gmail.com>
2016-11-17 10:13:22 -0800

    added initial index and pwp pages

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var mongoose = require('mongoose')
+var cors = require('cors')
+// App Constants
+var MENSAJE = process.env.MENSAJE
+
+var PORT = process.env.PORT || 3000
+// var DB_URL = 'mongodb://localhost/test'
+var DB_URL = 'mongodb://localhost/lpw_app'
+
+// console.log(MENSAJE)
+
+// Connect to DB
+mongoose.connect(DB_URL)
+
+// Setup Song Model
+var Book = mongoose.model('Song', {
+  title: String,
+  artist: String,
+  genre: String,
+  album: String,
+  quarter: String
+ })
+
+// Initialize your express app
+var app = express()
+
+// Set up middleware
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+// Set up Root Route
+app.get('/', function(request, response){
+  response.sendFile(__dirname + '/public/index.html')
+})
+
+
+
+app.listen(PORT, function(){
+  console.log("Server started on port", PORT + '!!!!!!!!!!!!!!')
+})
